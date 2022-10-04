5342017b8a6257e7a2e320e091e6afa71ac88e1a
Wissle <jongsome@gmail.com>
2022-10-04 18:53:53 +0900

    feat(Geom): Added xmonoElem and related functions.

--- paint-spill/Graphics/PaintSpill/Geom.hs
+++ paint-spill/Graphics/PaintSpill/Geom.hs
@@ -3,8 +3,60 @@
 
 module Graphics.PaintSpill.Geom where
 
-import Data.Semigroup (Arg (..))
+import Data.Maybe
 import Linear
+
+-- | Get y coordinate of a segment.
+segYUp :: (Ord a, Fractional a) => V2 a -> V2 a -> a -> a
+segYUp a b x
+  | ax == bx  = max ay by
+  | otherwise = (by - ay) / (bx - ax) * (x - ax) + ay
+  where
+    V2 ax ay = a
+    V2 bx by = b
+
+segYDown :: (Ord a, Fractional a) => V2 a -> V2 a -> a -> a
+segYDown a b x
+  | ax == bx  = min ay by
+  | otherwise = (by - ay) / (bx - ax) * (x - ax) + ay
+  where
+    V2 ax ay = a
+    V2 bx by = b
+
+-- | Get y coordinate of x-aligned strip
+xstripYUp :: (Ord a, Fractional a) => V2 a -> [V2 a] -> V2 a -> a -> a
+xstripYUp end strip start x
+  | ex <= x     = ey
+  | x <= sx     = sy
+  | otherwise   = go end strip start
+  where
+    V2 ex ey = end
+    V2 sx sy = start
+    go a [] s = segYUp s a x
+    go a (b : sn) s =
+        let V2 ax ay = a
+            V2 bx by = b
+        in  case compare bx x of
+                LT -> segYUp b a x
+                EQ -> if ax == x then segYUp b a x else go b sn s
+                _ -> go b sn s
+
+xstripYDown :: (Ord a, Fractional a) => V2 a -> [V2 a] -> V2 a -> a -> a
+xstripYDown end strip start x
+  | ex <= x     = ey
+  | x <= sx     = sy
+  | otherwise   = go end strip start
+  where
+    V2 ex ey = end
+    V2 sx sy = start
+    go a [] s = segYDown s a x
+    go a (b : sn) s =
+        let V2 ax ay = a
+            V2 bx by = b
+        in  case compare bx x of
+                LT -> segYDown b a x
+                EQ -> if ax == x then segYDown b a x else go b sn s
+                _ -> go b sn s
 
 -- | A Triangle type.
 -- 
@@ -21,3 +73,15 @@ triElem (Triangle a b c) e = (0 <= i) && (0 <= j) && (i + j <= 1)
     ae = e - a
     m = V2 ab ac
     V2 i j = ae *! inv22 m
+
+data XMonotone a = XMonotone a [a] [a] a
+
+-- | Get Y range of a x monotone.
+xmonoY :: (Ord a, Fractional a) => XMonotone (V2 a) -> a -> (a, a)
+xmonoY (XMonotone e u d s) x = (xstripYDown e d s x, xstripYUp e u s x)
+
+-- | Membership check for x monotone, as shape.
+xmonoElem :: (Ord a, Fractional a) => XMonotone (V2 a) -> V2 a -> Bool
+xmonoElem mt (V2 x y) = (l <= y) && (y <= h)
+  where
+    (l, h) = xmonoY mt x
--- test/Test/Geom.hs
+++ test/Test/Geom.hs
@@ -24,5 +24,105 @@ testGeom = testGroup "Geom"
                 triElem triangle (V2 1 1) @?= True 
                 triElem triangle (V2 0 0) @?= False
           ]
+      , testGroup "segY"
+          [ testGroup "Up"
+              [ testCase "A" (segYUp (V2 (-1) 0) (V2 1 0) 0 @?= 0)
+              , testCase "B" (segYUp (V2 0 0) (V2 2 2) 1 @?= 1)
+              , testCase "C" (segYUp (V2 1 1) (V2 1 1) 1 @?= 1)
+              , testCase "D" (segYUp (V2 0 0) (V2 0 2) 0 @?= 2)
+              ]
+          , testGroup "Down"
+              [ testCase "A" (segYDown (V2 (-1) 0) (V2 1 0) 0 @?= 0)
+              , testCase "B" (segYDown (V2 0 0) (V2 2 2) 1 @?= 1)
+              , testCase "C" (segYDown (V2 1 1) (V2 1 1) 1 @?= 1)
+              , testCase "D" (segYDown (V2 0 0) (V2 0 2) 0 @?= 0)
+              ]
+          ]
+
+      , testGroup "xstripY"
+          [ testGroup "Up"
+              [ testCase "OnSegment" $ do
+                    let start = V2 (-3) 0
+                        end = V2 3 1
+                        strip = [V2 1 0, V2 (-1) (-2)]
+                    xstripYUp end strip start 0 @?= (-1)
+              , testCase "OnPoint" $ do
+                    let start = V2 (-2) 0
+                        end = V2 4 1
+                        strip = [V2 2 0, V2 0 (-2)]
+                    xstripYUp end strip start 0 @?= -2
+              , testCase "OnVertical" $ do
+                    let start = V2 (-2) 0
+                        end = V2 2 1
+                        strip = [V2 0 0, V2 0 2]
+                    xstripYUp end strip start 0 @?= 2
+              ]
+
+          , testGroup "Down"
+              [ testCase "OnSegment" $ do
+                    let start = V2 (-3) 0
+                        end = V2 3 1
+                        strip = [V2 1 0, V2 (-1) (-2)]
+                    xstripYDown end strip start 0 @?= (-1)
+              , testCase "OnPoint" $ do
+                    let start = V2 (-2) 0
+                        end = V2 4 1
+                        strip = [V2 2 0, V2 0 (-2)]
+                    xstripYDown end strip start 0 @?= -2
+              , testCase "OnVertical" $ do
+                    let start = V2 (-2) 0
+                        end = V2 2 1
+                        strip = [V2 0 0, V2 0 2]
+                    xstripYDown end strip start 0 @?= 0
+              ]
+          ]
+
+      , testGroup "xmonoY"
+          [ testCase "Simple" $ do
+                let monotone = XMonotone (V2 1 0) [V2 0 1] [V2 0 (-1)] (V2 (-1) 0)
+                xmonoY monotone 0 @?= ((-1), 1)
+                xmonoY monotone 0.5 @?= ((-0.5), 0.5)
+          , testCase "End Point" $ do
+                let monotone = XMonotone (V2 1 0) [V2 0 1] [V2 0 (-1)] (V2 (-1) 0)
+                xmonoY monotone 1 @?= (0, 0)
+                xmonoY monotone (-1) @?= (0, 0)
+          , testCase "More" $ do
+                let monotone = XMonotone
+                        (V2 3 2)
+                        [V2 2 4, V2 (-1) 1]
+                        [V2 1 0, V2 0 0, V2 (-1) (-2)]
+                        (V2 (-2) (-2))
+                xmonoY monotone (-1.5) @?= ((-2), (-0.5))
+                xmonoY monotone (-0.5) @?= ((-1), 1.5)
+                xmonoY monotone 1 @?= (0, 3)
+          ]
+
+      , testGroup "monoElem"
+          [ testCase "Simple" $ do
+                let monotone = XMonotone (V2 1 0) [V2 0 1] [V2 0 (-1)] (V2 (-1) 0)
+                --  1 |    *
+                --  0 |  * x *
+                -- -1 |  x *
+                xmonoElem monotone (V2 0 0) @?= True
+                xmonoElem monotone (V2 (-1) (-1)) @?= False
+          , testCase "More" $ do
+                let monotone = XMonotone
+                        (V2 3 2)
+                        [V2 2 4, V2 (-1) 1]
+                        [V2 1 0, V2 0 0, V2 (-1) (-2)]
+                        (V2 (-2) (-2))
+                --  4 |     |   *
+                --  3 |     |
+                --  2 |     |     *
+                --  1 |   * |
+                --  0 | ----*-*----
+                -- -1 |     |
+                -- -2 | * * |
+                --     -2-1 0 1 2 3
+                xmonoElem monotone (V2 (-1) (-1)) @?= True
+                xmonoElem monotone (V2 2 0) @?= False
+                xmonoElem monotone (V2 2 2) @?= True
+                xmonoElem monotone (V2 (-1) 2) @?= False
+          ]
       ]
   ]
