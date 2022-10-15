d2fc5338c6aaaeebb5d9b2629c7f2c425ffad254
Wissle <jongsome@gmail.com>
2022-10-15 22:12:28 +0900

    feat(Geom): Implemented Monotone Decomposition.
    This will decomposite a simple polygon into X monotones.

--- test/Test/Geom/Polygon.hs
+++ test/Test/Geom/Polygon.hs
@@ -1,8 +1,10 @@
 module Test.Geom.Polygon where
 
+import Control.Applicative
+
 import Test.Tasty
 import Test.Tasty.HUnit
-import Test.Geom.Util
+import Test.Geom.Util ( ccoordList, (@#=?) )
 
 import Linear
 
@@ -43,5 +45,97 @@ testGroupPolygon = testGroup "Polygon"
             V2 0.5 1 `polyElem` poly @?= True
             V2 2 1 `polyElem` poly @?= False
       ]
-  , testGroup "Monotone Decomposition" []
+  , testGroup "Monotone Decomposition"
+      [ testCase "Triangle" $ do
+            let poly = [V2 0 0, V2 1 0, V2 0 1]
+                monos = fmap (fmap snd) $ monotoneDecomp (zip [ 0 .. ] poly)
+            
+                polyMap = flip polyElem poly <$> ccoordList
+                monoMaps = fmap (\m -> fmap (xmonoElem m) ccoordList) monos
+                monoMap = foldr (liftA2 (||)) (False <$ ccoordList) monoMaps
+
+                npolyverts = length poly
+                nmonoverts = sum (fmap (\(XMonotone _ s _) -> length s + 2) monos) - (length monos * 2) + 2
+            
+            polyMap @#=? monoMap
+            npolyverts @=? nmonoverts
+            
+      , testCase "Vase shaped" $ do
+            let poly = [V2 0 0, V2 3 0, V2 2 1, V2 3 2, V2 0 2, V2 1 1]
+                monos = fmap (fmap snd) $ monotoneDecomp (zip [ 0 .. ] poly)
+            
+                polyMap = flip polyElem poly <$> ccoordList
+                monoMaps = fmap (\m -> fmap (xmonoElem m) ccoordList) monos
+                monoMap = foldr (liftA2 (||)) (False <$ ccoordList) monoMaps
+
+                npolyverts = length poly
+                nmonoverts = sum (fmap (\(XMonotone _ s _) -> length s + 2) monos) - (length monos * 2) + 2
+            
+            polyMap @#=? monoMap
+            npolyverts @=? nmonoverts
+            
+      , testCase "Clover" $ do
+            let poly = [V2 0 0, V2 1 0, V2 2 1, V2 3 0, V2 4 0, V2 4 1, V2 3 2, V2 4 3, V2 4 4, V2 3 4, V2 2 3, V2 1 4, V2 0 4, V2 0 3, V2 1 2, V2 0 1]
+                monos = fmap (fmap snd) $ monotoneDecomp (zip [ 0 .. ] poly)
+            
+                polyMap = flip polyElem poly <$> ccoordList
+                monoMaps = fmap (\m -> fmap (xmonoElem m) ccoordList) monos
+                monoMap = foldr (liftA2 (||)) (False <$ ccoordList) monoMaps
+
+                npolyverts = length poly
+                nmonoverts = sum (fmap (\(XMonotone _ s _) -> length s + 2) monos) - (length monos * 2) + 2
+
+            print monos
+
+            polyMap @#=? monoMap
+            npolyverts @=? nmonoverts
+            
+      , testCase "ZigZags" $ do
+            let poly = [V2 0 0, V2 2 0, V2 4 1, V2 2 2, V2 4 3, V2 2 4, V2 4 5, V2 2 6, V2 4 7, V2 2 7, V2 0 6, V2 2 5, V2 0 4, V2 2 3, V2 0 2, V2 2 1]
+                monos = fmap (fmap snd) $ monotoneDecomp (zip [ 0 .. ] poly)
+            
+                polyMap = flip polyElem poly <$> ccoordList
+                monoMaps = fmap (\m -> fmap (xmonoElem m) ccoordList) monos
+                monoMap = foldr (liftA2 (||)) (False <$ ccoordList) monoMaps
+
+                npolyverts = length poly
+                nmonoverts = sum (fmap (\(XMonotone _ s _) -> length s + 2) monos) - (length monos * 2) + 2
+
+            print monos
+
+            polyMap @#=? monoMap
+            npolyverts @=? nmonoverts
+            
+      , testCase "ZigZags 2" $ do
+            let poly = [V2 0 0, V2 3 0, V2 5 1, V2 3 2, V2 5 3, V2 3 4, V2 5 5, V2 3 6, V2 5 7, V2 2 7, V2 0 6, V2 2 5, V2 0 4, V2 2 3, V2 0 2, V2 2 1]
+                monos = fmap (fmap snd) $ monotoneDecomp (zip [ 0 .. ] poly)
+            
+                polyMap = flip polyElem poly <$> ccoordList
+                monoMaps = fmap (\m -> fmap (xmonoElem m) ccoordList) monos
+                monoMap = foldr (liftA2 (||)) (False <$ ccoordList) monoMaps
+
+                npolyverts = length poly
+                nmonoverts = sum (fmap (\(XMonotone _ s _) -> length s + 2) monos) - (length monos * 2) + 2
+
+            print monos
+
+            polyMap @#=? monoMap
+            npolyverts @=? nmonoverts
+            
+      , testCase "Swirl Shape" $ do
+            let poly = [V2 0 0, V2 2 0, V2 3 2, V2 4 0, V2 4 2, V2 2 3, V2 4 4, V2 2 4, V2 1 2, V2 0 4, V2 0 2, V2 2 1]
+                monos = fmap (fmap snd) $ monotoneDecomp (zip [ 0 .. ] poly)
+            
+                polyMap = flip polyElem poly <$> ccoordList
+                monoMaps = fmap (\m -> fmap (xmonoElem m) ccoordList) monos
+                monoMap = foldr (liftA2 (||)) (False <$ ccoordList) monoMaps
+
+                npolyverts = length poly
+                nmonoverts = sum (fmap (\(XMonotone _ s _) -> length s + 2) monos) - (length monos * 2) + 2
+
+            print monos
+
+            polyMap @#=? monoMap
+            npolyverts @=? nmonoverts
+      ]
   ]
